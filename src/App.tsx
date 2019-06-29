319417dd10bfa7435398fbf6feb48206eb523a0a
Sekib Omazic <sekib.omazic@gmail.com>
2019-06-29 21:42:25 +0200

    add disabled attribute

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: FC = () => {
   const objective = {
     name: "Sekib",
     amount: 500,
-    status: ObjectiveStatus.Approved,
+    status: ObjectiveStatus.Proposal,
     keyResults: [
       {
         title: "Learn React hooks",
--- src/okr/ObjectiveComponent.tsx
+++ src/okr/ObjectiveComponent.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import { Field } from "formik";
 import { Input } from "./Input";
-import { Objective } from "../types";
+import { Objective, ObjectiveStatus } from "../types";
 
 interface Props {
   objective: Objective;
@@ -10,7 +10,12 @@ export const ObjectiveComponent: FC<Props> = ({ objective }) => {
   return (
     <div className="mb4">
       <h1>Objective</h1>
-      <Field name="name" value={objective.name} component={Input} />
+      <Field
+        name="name"
+        value={objective.name}
+        disabled={objective.status === ObjectiveStatus.Approved}
+        component={Input}
+      />
     </div>
   );
 };
